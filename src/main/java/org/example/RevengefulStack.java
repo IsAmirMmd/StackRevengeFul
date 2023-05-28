f0d1746841d74cc6422c3c5d4e75c5aff569f593
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-05-28 18:36:08 +0330

    first commit

---
+++ src/main/java/org/example/RevengefulStack.java
@@ -0,0 +1,96 @@
+package org.example;
+
+
+import java.util.LinkedList;
+
+public class RevengefulStack<T> {
+    private LinkedList<T> stack;
+    private int capacity;
+
+    public RevengefulStack(int initialCapacity) {
+        stack = new LinkedList<>();
+        capacity = initialCapacity;
+    }
+
+    public void push(T item) {
+//        System.out.println("cap : "+capacity);
+//        System.out.println("size : "+stack.size());
+        int temp = capacity;
+        capacity /= 2;
+        if (stack.size() >= capacity) {
+//            System.out.println("cap : "+temp);
+//            System.out.println("size : "+stack.size());
+            if (temp == stack.size()) {
+                throw new RuntimeException("I will no longer bow down to you!");
+            }
+            throw new RuntimeException("No. I won't let you.");
+        }
+        stack.push(item);
+    }
+
+    public T pop() {
+        capacity /= 2;
+        if (stack.isEmpty()) {
+            throw new RuntimeException("I have nothing to give you.");
+        }
+        return stack.pop();
+    }
+
+    public int getSize() {
+        return stack.size();
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void printStack() {
+        if (capacity <= stack.size()) {
+            return;
+        }
+
+        capacity /= 2;
+
+        for (T item : stack) {
+            System.out.println(item);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        RevengefulStack<Integer> stack = new RevengefulStack<>(30);
+        System.out.println("Capacity = " + stack.getCapacity() + ", Size = " + stack.getSize());
+
+        try {
+            stack.push(1);
+            System.out.println("Capacity = " + stack.getCapacity() + ", Size = " + stack.getSize());
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            stack.push(2);
+            System.out.println("Capacity = " + stack.getCapacity() + ", Size = " + stack.getSize());
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        try {
+            stack.pop();
+            System.out.println("Capacity = " + stack.getCapacity() + ", Size = " + stack.getSize());
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            stack.push(3);
+            System.out.println("Capacity = " + stack.getCapacity() + ", Size = " + stack.getSize());
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        stack.printStack();
+//        System.out.println("Capacity = " + stack.getCapacity() + ", Size = " + stack.getSize());
+    }
+}
